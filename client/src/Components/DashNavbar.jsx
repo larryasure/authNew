1c9376925be960334adc6a231285adb98af51278
Larryasure <olascolanre@gmail.com>
2026-03-23 11:03:58 +0100

    Notification page keeling me

--- client/src/Components/DashNavbar.jsx
+++ client/src/Components/DashNavbar.jsx
@@ -1,4 +1,4 @@
-import { Bell, LogOut, Menu, Settings, X } from "lucide-react";
+import { Bell, Disc, LogOut, Menu, Settings, X } from "lucide-react";
 import React, { useEffect, useState, useRef, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../Context/AuthContext";
@@ -96,71 +96,92 @@ export default function DashNavbar({ budgets = [], transactions = [] }) {
         <div className=" flex items-center  gap-6 cursor-pointer">
           <div className="relative" ref={notifRef}>
             <button
-              onClick={() => setIsNotifOpen(!notifRef)}
+              onClick={() => setIsNotifOpen(!isNotifOpen)}
               className="relative text-gray-700 hover:text-[var(--brand)] "
             >
               <Bell className="w-6 h-6 cursor-pointer" />
               {displayUnreadCount > 0 && (
-                  <span className="w-3 h-3 absolute text-xs bg-red-500 text-white flex items-center justify-center rounded-full -top-2 -right-1">
-                  { displayUnreadCount}
-              </span>
-
+                <span className="w-3 h-3 absolute text-xs bg-red-500 text-white flex items-center justify-center rounded-full -top-2 -right-1">
+                  {displayUnreadCount}
+                </span>
               )}
-            
-                
+            </button>
+            {isNotifOpen && (
+              <div className="absolute w-80 mt-3 right-0 bg-white border border-gray-200 shadow-xl z-50 rounded-xl  ">
+                <div className="flex justify-between items-center border-b border-gray-100 px-4 py-2.5 ">
+                  <h3 className="font-bold text-sm text-gray-800">
+                    Notifications
+                  </h3>
+                  <button
+                    onClick={markAllread}
+                    className="text-xs font-semibold text-[var(--brand)] hover:underline"
+                  >
+                    Mark all as read
+                  </button>
+                </div>
+                <div className="flex border-b border-gray-100 ">
+                  {["tab", "unread", "system"].map((tab) => (
+                    <button
+                      key={tab}
+                      onClick={() => setActiveTabs(tab)}
+                      className={`flex-1 py-2 font-medium transition-all capitalize duration-200 ${activeTabs === tab ? "text-[var(--brand)] border-b-2 border-[var(--brand)] " : "text-gray-400 hover:text-gray-600"} `}
+                    >
+                      {activeTabs === "unread"
+                        ? `Unread (${displayUnreadCount})`
+                        : tab}
+                    </button>
+                  ))}
+                </div>
+
+                <div>
+                  
+                </div>
+              </div>
+            )}
+          </div>
+
+          <div ref={profileRef} className="relative  ">
+            <button
+              onClick={() => setIsProfileOpen(!isProfileOpen)}
+              className="px-3 py-1.5 gap-2 flex items-center rounded-lg hover:bg-gray-100 transition-all duration-200 cursor-pointer active:scale-105"
+            >
+              <div className="w-8 h-8  bg-[var(--brand)] rounded-full text-white  font-bold flex items-center justify-center">
+                {user?.name
+                  ?.split(" ")
+                  .map((n) => n[0])
+                  .join("")
+                  .toUpperCase() || "U"}
+              </div>
+              <span className="text-sm font-semibold  hidden sm:block">
+                {user?.name || "User"}
+              </span>
             </button>
 
-            <div ref={profileRef} className="relative  ">
-              <button
-                onClick={() => setIsProfileOpen(!isProfileOpen)}
-                className="px-3 py-1.5 gap-2 flex items-center rounded-lg hover:bg-gray-100 transition-all duration-200 cursor-pointer active:scale-105"
-              >
-                <div className="w-8 h-8  bg-[var(--brand)] rounded-full text-white  font-bold flex items-center justify-center">
-                  {user?.name
-                    ?.split(" ")
-                    .map((n) => n[0])
-                    .join("")
-                    .toUpperCase() || "U"}
-                </div>
-                <span className="text-sm font-semibold  hidden sm:block">
-                  {user?.name || "User"}
-                </span>
-              </button>
-
-              {isProfileOpen && (
-                <div className="absolute z-10 right-0 mt-2 border border-gray-300 w-48 rounded-xl bg-white shadow-lg">
-                  {/* <button
-                  onClick={() => { navigate("/dashboard/settings");  setIsProfileOpen(false)}}
+            {isProfileOpen && (
+              <div className="absolute z-10 right-0 mt-2 border border-gray-300 w-48 rounded-xl bg-white shadow-lg">
+                <button
+                  onClick={() => {
+                    navigate("/dashboard/settings");
+                    setIsProfileOpen(false);
+                  }}
                   className="w-full flex items-center gap-4 px-4 py-2 hover:bg-gray-100 border-b border-gray-200 transition-all duration-300 cursor-pointer"
                 >
-                  <User className="w-4 h-4" />
-                  Profile
-                </button> */}
+                  <Settings className="w-4 h-4" />
+                  Settings
+                </button>
 
-                  <button
-                    onClick={() => {
-                      navigate("/dashboard/settings");
-                      setIsProfileOpen(false);
-                    }}
-                    className="w-full flex items-center gap-4 px-4 py-2 hover:bg-gray-100 border-b border-gray-200 transition-all duration-300 cursor-pointer"
-                  >
-                    <Settings className="w-4 h-4" />
-                    Settings
-                  </button>
-
-                  <button
-                    onClick={() => {
-                      navigate("/dashboard/logout");
-                      setIsProfileOpen(false);
-                    }}
-                    className="w-full flex items-center gap-4 px-4 py-2 hover:bg-gray-100 text-red-600 transition-all duration-300 cursor-pointer "
-                  >
-                    <LogOut className="w-4 h-4" />
-                    Logout
-                  </button>
-                </div>
-              )}
-            </div>
+                <button
+                  onClick={() => {
+                    navigate("/dashboard/logout");
+                    setIsProfileOpen(false);
+                  }}
+                  className="w-full flex items-center gap-4 px-4 py-2 hover:bg-gray-100 text-red-600 transition-all duration-300 cursor-pointer "
+                >
+                  <LogOut className="w-4 h-4" />
+                  Logout
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </nav>
